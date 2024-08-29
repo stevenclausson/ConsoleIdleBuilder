efce49c854753992625dada8a878824e8ca8b246
Steven <sclausson@gmail.com>
2024-08-28 23:36:43 -0700

    - Added intro - Started Database Initialization

---
+++ ConsoleIdleBuilder/DatabaseHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace ConsoleIdleBuilder
+{
+    class DatabaseHelper
+    {
+        public static string connectionString = @"Data Source=..\..\..\Files\IdleBuilder.db;Version=3";
+
+        public static void InitializeDatabase()
+        {
+            if (!File.Exists(@"..\..\..\Files\IdleBuidler.db"))
+            {
+                SQLiteConnection.CreateFile(@"..\..\..\Files\IdleBuilder.db");
+
+                using (var connection = new SQLiteConnection(connectionString))
+                {
+                    connection.Open();
+                    string createCityTableQuery = @"
+                        CREATE TABLE IF NOT EXISTS city (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        name TEXT NOT NULL,
+                        size INTEGER NOT NULL
+                        )";
+
+                    string createResourcesTableQuery = @"
+                        CREATE TABLE IF NOT EXISTS resources (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        name TEXT NOT NULL,
+                        quantity INTEGER NOT NULL,
+                        type TEXT NOT NULL
+                        )";
+
+                    using (var command = new SQLiteCommand(connection)) 
+                    {
+                        command.CommandText = createCityTableQuery;
+                        command.ExecuteNonQuery();
+
+                        command.CommandText += createResourcesTableQuery;
+                        command.ExecuteNonQuery();
+                    }
+                        
+                }
+            }
+        }
+    }
+}
--- ConsoleIdleBuilder/Program.cs
+++ ConsoleIdleBuilder/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleIdleBuilder;
+using System.Data.SQLite;
+
+Console.WriteLine("Welcome to Idle Builder!");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("version: 0.0.1");
+Console.ForegroundColor= ConsoleColor.White;
+Console.WriteLine("\n\nPlease wait while the database initializes...");
+//DatabaseHelper.InitializeDatabase();
+Console.ForegroundColor= ConsoleColor.Green;
+Console.WriteLine("\nDatabase Created!");
+Console.ForegroundColor= ConsoleColor.White;
+Console.WriteLine("Press any key to begin game!");
+Console.ReadLine();
